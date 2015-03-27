7612734e37769f89eb9d3046f22f91e1f25b2124
Daniel Colino Fernández <daniel.colino@egibide.org>
2015-03-27 21:22:06 +0100

    2.Carga inicial del projecto

---
+++ src/ejemplogit/EjemploGit.java
@@ -0,0 +1,15 @@
+package ejemplogit;
+
+public class EjemploGit {
+
+    public static void main(String[] args) {
+
+        //Escribe aquí...
+        
+        System.out.println("Este es un projecto para probar GIT..");
+
+
+
+    }
+
+}
